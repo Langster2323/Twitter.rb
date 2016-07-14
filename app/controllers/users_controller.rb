cdfbbd80da353370ed9dcdb7da536a0c69d1dffc
Ahkeem Lang <langahkeem@gmail.com>
2016-07-14 17:18:12 -0500

    just finished my create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,9 @@ class UsersController < ApplicationController
   def show
     binding.pry
     if User.exists?(params[:id])
-    render template: 'users/show.html.erb', locals: { user: User.find(params[:id]) }
+      render template: 'users/show.html.erb', locals: { user: User.find(params[:id]) }
     else
-      render html: "Not found" status: 404
+      render html: "Not found", status: 404
     end
   end
 
@@ -27,7 +27,17 @@ class UsersController < ApplicationController
   end
 
   def create
+    user = User.new
+    user.fullname = params[:user][:fullname]
+    if user.save
+      redirect_to_user_path(user)
+    else
+      flash[:alert] = "Could not be saved due to errors"
+      render template: 'users/new.html.erb', locals: {
+        user: user
+      }
 
+    end
   end
 
   def edit
