684ec5919b71f7ecd6c779db08774e9c2c8b7173
Ahkeem Lang <langahkeem@gmail.com>
2016-07-15 13:41:45 -0500

    Finished working on the Tweet controller for now.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,16 +1,56 @@
 class TweetsController < ApplicationController
-  def index
-  end
+    def index
+      render template: 'tweets/index.html.erb', locals: {
+        tweets: Tweet.all,
 
-  def show
-  end
+      }
+    end
 
-  def new
-  end
+    def show
+      if Tweet.exists?(params[:id])
+        tweet = Tweet.find(params[:id])
+        render template: 'tweets/show.html.erb', locals: {
+          body: body
+        }
+      else
+        render html: "Not Found", status: 404
+      end
+    end
 
-  def create
-  end
+    def new
+      render locals: {
+        tweet: Tweet.new
+      }
+    end
 
-  def update
-  end
+    def create
+      tweet = Tweet.new
+      tweet.body = params[:tweet][:body]
+      if tweet.save
+        redirect_to_tweet_path(tweet)
+      else
+        flash[:alert] = "Could not be saved due to errors."
+        render template: '/tweets/new.html.erb', locals: {
+          tweet: tweet
+        }
+      end
+    end
+
+    def edit
+      render locals: {
+        tweet: Tweet.find(params[:id])
+      }
+    end
+
+    def update
+      tweet = Tweet.find(params[:id])
+      tweet.body = params[:tweet][:body] if params[:tweet][:body].present?
+      if tweet.save
+        redirect_to_tweet_path(tweet)
+      else
+        render template: '/tweets/new.html.erb', locals: {
+          tweet: tweet
+        }
+      end
+    end
 end
